fd599b13426aae4893630c20c7b27b2579b85b1d
Adrianderson Lira <43156311+AdriandersonLira@users.noreply.github.com>
2023-05-11 01:01:20 -0300

    Update Order.java

--- src/app/Order.java
+++ src/app/Order.java
@@ -7,8 +7,6 @@ import java.util.concurrent.TimeUnit;
 public class Order implements State {
     private State state;
 
-    private boolean isProblem = false;
-
     public Order() {
         this.state = new Unprocessed(this);
     }
@@ -24,10 +22,6 @@ public class Order implements State {
 
     public void setState(State state) {
         this.state = state;
-    }
-
-    public void setProblem(boolean problem) {
-        isProblem = problem;
     }
 
     public void refreshStatus() {
